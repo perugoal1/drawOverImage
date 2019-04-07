a4f0a7aedfab99634a5d3f07a4dcf12ca48b77f7
DESKTOP-5CEQE1F\user <perugoal2@gmail.com>
2019-04-08 01:01:27 +0530

    integrating graphwl

---
+++ LambdaCode/types.js
@@ -0,0 +1,29 @@
+const graphql = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLBoolean
+} = graphql;
+
+
+const Images = new GraphQLObjectType({
+  name: 'Images',
+  fields: () => ( {
+      id: { type: GraphQLID },
+      name: { type:  GraphQLString },
+      overlay :{ type:  GraphQLString },
+      width :{ type:  GraphQLString },
+      height :{ type:  GraphQLString },
+      url : { type:  GraphQLString }
+   }) 
+});
+
+
+
+
+
+
+
